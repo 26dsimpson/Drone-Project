019f477e2f03a2aa50df0f560d432ce4ef1d8f0a
MS CSLab <mscslab@woodward.edu>
2022-04-08 13:48:43 -0400

    Added package

---
+++ telloDanielle.js
@@ -0,0 +1,10 @@
+var msg = "Mr Stewart says";
+ 
+function name (msg){
+    console.log(msg +"Hello World")
+}
+
+
+name(msg)
+
+// Created a functionn to make sure the program says"Mr. Stewart says Hello World".
